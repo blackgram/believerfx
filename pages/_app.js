c595986dce15ff50bd7e7c85c0104e0fc79f9376
blackgram <ajiriokpeva@gmail.com>
2024-02-16 16:10:54 +0100

    meta-theme-color

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import { store } from "../Redux/store";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+    <Head>
+    <meta name="theme-color" content="#000" />
+    </Head>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 }
